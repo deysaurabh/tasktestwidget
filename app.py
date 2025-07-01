56fe748e60663a8ee4f7e9dafd8fa93f7a1065ab
deysaurabh <saurabh.dey@gmail.com>
2025-06-30 23:38:13 -0700

    enclosed the flask app in a create app function call

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 from flask import Flask, render_template, request
 from datetime import datetime
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    code_snippet = ""
-    if request.method == 'POST':
-        code_snippet = request.form.get('code', '')
-    return render_template('index.html', code_snippet=code_snippet, current_date=datetime.now().strftime('%Y-%m-%d'))
+    @app.route('/', methods=['GET', 'POST'])
+    def index():
+        code_snippet = ""
+        if request.method == 'POST':
+            code_snippet = request.form.get('code', '')
+        return render_template('index.html', code_snippet=code_snippet, current_date=datetime.now().strftime('%Y-%m-%d'))
 
-if __name__ == '__main__':
-    import os
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    if __name__ == '__main__':
+        import os
+        port = int(os.environ.get('PORT', 5000))
+        app.run(host='0.0.0.0', port=port, debug=False)
+
+    return app
