f2b08b973458fc121447fd6ff3f4f65f77bfe588
deysaurabh <saurabh.dey@gmail.com>
2025-07-01 11:22:42 -0700

    Update app for Python 3.8 compatibility

--- app.py
+++ app.py
@@ -1,19 +1,18 @@
 from flask import Flask, render_template, request
 from datetime import datetime
 
-def create_app():
-    app = Flask(__name__)
 
-    @app.route('/', methods=['GET', 'POST'])
-    def index():
-        code_snippet = ""
-        if request.method == 'POST':
-            code_snippet = request.form.get('code', '')
-        return render_template('index.html', code_snippet=code_snippet, current_date=datetime.now().strftime('%Y-%m-%d'))
+app = Flask(__name__)
 
-    if __name__ == '__main__':
-        import os
-        port = int(os.environ.get('PORT', 5000))
-        app.run(host='0.0.0.0', port=port, debug=False)
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    code_snippet = ""
+    if request.method == 'POST':
+        code_snippet = request.form.get('code', '')
+    return render_template('index.html', code_snippet=code_snippet, current_date=datetime.now().strftime('%Y-%m-%d'))
 
-    return app
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
+
